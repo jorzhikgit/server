06f704c29d47035841ceae20230281c8fed44ef5
Matthew Cantelon <me@matthewcantelon.ca>
2015-06-24 09:27:10 -0700

    Code documentation

--- playerInteractor.go
+++ playerInteractor.go
@@ -4,12 +4,16 @@ type PlayerInteractor struct {
 	playerRepo PlayerRepository
 }
 
+// NewPlayerInteractor creates a new PlayerInteractor
 func NewPlayerInteractor(pr PlayerRepository) PlayerInteractor {
 	return PlayerInteractor{
 		playerRepo: pr,
 	}
 }
 
+// CreateNewPlayer creates a new player using a username and a flag to determine
+// host status.
+// An empty player is returned if there is an error
 func (p *PlayerInteractor) CreateNewPlayer(name string, isHost bool) Player {
 	player := Player{
 		Name:   name,
@@ -26,6 +30,9 @@ func (p *PlayerInteractor) CreateNewPlayer(name string, isHost bool) Player {
 	return player
 }
 
+// CreateHost takes a username and creates a new host. This is a shortcut
+// to `CreateNewPlayer` to reduce magical values throughout the codebase.
+// An empty Player is returned if there is an error
 func (p *PlayerInteractor) CreateHost(name string) Player {
 	return p.CreateNewPlayer(name, true)
 }
