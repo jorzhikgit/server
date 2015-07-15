ee197692291e235d5fb055832f87b178a0f96ee0
Matthew Cantelon <me@matthewcantelon.ca>
2015-07-15 07:28:37 -0700

    Update `FakeConnection` to match latest interface

--- connection_test.go
+++ connection_test.go
@@ -6,5 +6,6 @@ import (
 
 type FakeConnection struct{}
 
-func (fc *FakeConnection) Read() error  { return nil }
-func (fc *FakeConnection) Write() error { return nil }
+func (fc *FakeConnection) Read() error             { return nil }
+func (fc *FakeConnection) Write(Event Event) error { return nil }
+func (fc *FakeConnection) CloseChannel() error     { return nil }
